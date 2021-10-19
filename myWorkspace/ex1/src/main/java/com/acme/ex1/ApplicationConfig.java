3aaeaf020cd70af6da929758af7c72778fbdadfc
hschoonjans <hschoonjans@cirb.brussels>
2021-10-19 17:39:51 +0200

    Exercise 1.2: annotates ApplicationConfig with @SpringBootApplication

--- myWorkspace/ex1/src/main/java/com/acme/ex1/ApplicationConfig.java
+++ myWorkspace/ex1/src/main/java/com/acme/ex1/ApplicationConfig.java
@@ -4,24 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+@SpringBootApplication
 public class ApplicationConfig {
 
-	@Bean
-	@Scope("prototype")
-	Logger logger(InjectionPoint ip) {
-		return LoggerFactory.getLogger(ip.getMember().getDeclaringClass());
-	}
-	
-	public static void main(String[] args)  /*just to check if application context contains required beans*/{
-		var ctx = SpringApplication.run(ApplicationConfig.class, args);
-		System.out.println("ctx is open : "+ctx);
-		System.out.println(ctx.getBeansOfType(javax.sql.DataSource.class));
-		System.out.println(ctx.getBeansOfType(javax.persistence.EntityManagerFactory.class));
-		//System.out.println(ctx.getBeansOfType(org.springframework.transaction.PlatformTransactionManager.class));
-		System.out.println("closing");
-		ctx.close();
-	}
+    @Bean
+    @Scope("prototype")
+    Logger logger(InjectionPoint ip) {
+        return LoggerFactory.getLogger(ip.getMember().getDeclaringClass());
+    }
+
+    public static void main(String[] args)  /*just to check if application context contains required beans*/ {
+        var ctx = SpringApplication.run(ApplicationConfig.class, args);
+        System.out.println("ctx is open : " + ctx);
+        System.out.println(ctx.getBeansOfType(javax.sql.DataSource.class));
+        System.out.println(ctx.getBeansOfType(javax.persistence.EntityManagerFactory.class));
+        //System.out.println(ctx.getBeansOfType(org.springframework.transaction.PlatformTransactionManager.class));
+        System.out.println("closing");
+        ctx.close();
+    }
 }
