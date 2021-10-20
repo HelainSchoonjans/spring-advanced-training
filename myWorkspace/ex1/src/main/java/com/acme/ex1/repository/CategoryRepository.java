645ce58444dd478a1a9addb44ce5c07dac6951c8
hschoonjans <hschoonjans@cirb.brussels>
2021-10-20 13:46:37 +0200

    All repositories are jpa repositories

--- myWorkspace/ex1/src/main/java/com/acme/ex1/repository/CategoryRepository.java
+++ myWorkspace/ex1/src/main/java/com/acme/ex1/repository/CategoryRepository.java
@@ -1,7 +1,7 @@
 package com.acme.ex1.repository;
 
 import com.acme.ex1.model.Category;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends CrudRepository<Category, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
