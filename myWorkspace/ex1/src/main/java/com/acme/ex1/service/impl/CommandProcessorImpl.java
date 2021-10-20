5e44fc534cb936bd934924aae3dd402c2f03f703
hschoonjans <hschoonjans@cirb.brussels>
2021-10-20 13:59:32 +0200

    Correction

--- myWorkspace/ex1/src/main/java/com/acme/ex1/service/impl/CommandProcessorImpl.java
+++ myWorkspace/ex1/src/main/java/com/acme/ex1/service/impl/CommandProcessorImpl.java
@@ -16,56 +16,57 @@ import javax.transaction.Transactional;
 @Service
 public class CommandProcessorImpl implements CommandProcessor {
 
-	private Logger logger;
+    private Logger logger;
 
-	private List<ExceptionHandler> exHandlers;
+    private List<ExceptionHandler> exHandlers;
 
-	private ApplicationContext ctx;
+    private ApplicationContext ctx;
 
-	public CommandProcessorImpl(Logger logger, List<ExceptionHandler> exHandlers, ApplicationContext ctx) {
-		super();
-		this.logger = logger;
-		this.exHandlers = exHandlers;
-		this.ctx = ctx;
-	}
+    public CommandProcessorImpl(Logger logger, List<ExceptionHandler> exHandlers, ApplicationContext ctx) {
+        super();
+        this.logger = logger;
+        this.exHandlers = exHandlers;
+        this.ctx = ctx;
+    }
 
-	@Transactional
-	@Override
-	public <T extends AbstractCommand> T process(T command) {
+    // transaction required?
+    @Transactional
+    @Override
+    public <T extends AbstractCommand> T process(T command) {
 
-		String cmdId = command.toString();
-		logger.info("Received a command of type {} : {}", command.getClass().getSimpleName(), cmdId);
+        String cmdId = command.toString();
+        logger.info("Received a command of type {} : {}", command.getClass().getSimpleName(), cmdId);
 
-		try {
-			logger.info("Is {} valid before handling ?", cmdId);
-			command.validateStateBeforeHandling();
-			logger.info("yes, {} is valid, return it", cmdId);
-			
-			// dispatch command to business layer (command handlers)
-			ctx.publishEvent(command);
-			
-			logger.info("is {} valid after handling ?", cmdId);
-			command.validateStateAfterHandling();
-			logger.info("is {} valid after handling ?", cmdId);
+        try {
+            logger.info("Is {} valid before handling ?", cmdId);
+            command.validateStateBeforeHandling();
+            logger.info("yes, {} is valid, return it", cmdId);
 
-			return command;
-		} catch (Exception e) {
+            // dispatch command to business layer (command handlers)
+            // dispatched to listeners that declare they can handle given command type
+            ctx.publishEvent(command);
+
+            logger.info("is {} valid after handling ?", cmdId);
+            command.validateStateAfterHandling();
+            logger.info("is {} valid after handling ?", cmdId);
+
+            return command;
+        } catch (Exception e) {
 			/*exemple de traitement d'exception : 
 			 serialisation de la commande dans un fichier sous le nom cmd.toString()+".json"
 			 et écriture de la stacktrace dans un fichier sous le nom cmd.toString()+".stacktrace.txt"
 			 Voir DefaultExceptionHandler
 			*/
-			exHandlers.forEach(x -> x.handleException(e, command));
-			
-			if (e instanceof CommandException) {
-				logger.warn("hum hum... "+e.getMessage());
-				throw e;
-			}
-			else{
-				e.printStackTrace();
-				logger.error("hum hum... "+e.getMessage());
-				throw new RuntimeException("Oops, I did it again...");
-			}
-		}
-	}
+            exHandlers.forEach(x -> x.handleException(e, command));
+
+            if (e instanceof CommandException) {
+                logger.warn("hum hum... " + e.getMessage());
+                throw e;
+            } else {
+                e.printStackTrace();
+                logger.error("hum hum... " + e.getMessage());
+                throw new RuntimeException("Oops, I did it again...");
+            }
+        }
+    }
 }
