c880ba41172047e38c3e1ffcd18192f3385a5ac2
hschoonjans <hschoonjans@cirb.brussels>
2021-10-20 11:46:23 +0200

    Member repository cleanup

--- myWorkspace/ex1/src/main/java/com/acme/ex1/repository/MemberRepository.java
+++ myWorkspace/ex1/src/main/java/com/acme/ex1/repository/MemberRepository.java
@@ -1,11 +1,7 @@
 package com.acme.ex1.repository;
 
-import com.acme.ex1.model.Author;
 import com.acme.ex1.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
