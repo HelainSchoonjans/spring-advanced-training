944d1ce7a91d267b9ce24b0547a83c100b534478
hschoonjans <hschoonjans@cirb.brussels>
2021-10-20 14:47:02 +0200

    Can find books with part of the title

--- myWorkspace/ex1/src/main/java/com/acme/ex1/repository/BookRepository.java
+++ myWorkspace/ex1/src/main/java/com/acme/ex1/repository/BookRepository.java
@@ -3,5 +3,9 @@ package com.acme.ex1.repository;
 import com.acme.ex1.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    List<Book> findAllByTitleLike(String title);
 }
--- myWorkspace/ex1/src/main/java/com/acme/ex1/web/controller/BookController.java
+++ myWorkspace/ex1/src/main/java/com/acme/ex1/web/controller/BookController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import com.acme.ex1.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,36 +16,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.acme.ex1.model.Book;
 import com.acme.ex1.service.command.ReservationCommand;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class BookController {
 
-	@Autowired
-	private BookRepository bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
-	@GetMapping("books")
-	String list(Map<String, Object> model) {
-		model.put("probe", new Book());
-		return "books/list";
-	}
+    @GetMapping("books")
+    String list(Map<String, Object> model) {
+        model.put("probe", new Book());
+        return "books/list";
+    }
 
-	@GetMapping(path = "books", params = "title")
-	String list(@ModelAttribute("probe") Book probe, Map<String, Object> model) {
-		List<Book> results = bookRepository.findAll(Example.of(probe));
-		
-		model.put("results", results);
-		return "books/list";
-	}
+    @GetMapping(path = "books", params = "title")
+    String list(@ModelAttribute("probe") Book probe, Map<String, Object> model, @RequestParam String title) {
+        List<Book> results = bookRepository.findAll(Example.of(probe));
+        if (results.isEmpty() && !title.isEmpty()) {
+            results = bookRepository.findAllByTitleLike("%" + title + "%");
+        }
 
-	@GetMapping("books/{id}")
-	String book(@PathVariable int id, Map<String, Object> model) {
-		Optional<Book> maybeBook = bookRepository.findById(id);
-		maybeBook.ifPresent(b -> model.put("entity", b));
-		
-		model.putIfAbsent("command", new ReservationCommand());
-		return "books/detail";
-	}
-	
+        model.put("results", results);
+        return "books/list";
+    }
+
+    @GetMapping("books/{id}")
+    String book(@PathVariable int id, Map<String, Object> model) {
+        Optional<Book> maybeBook = bookRepository.findById(id);
+        maybeBook.ifPresent(b -> model.put("entity", b));
+
+        model.putIfAbsent("command", new ReservationCommand());
+        return "books/detail";
+    }
+
 }
 
