c7dbf1e90ba96fcfd5b0deefe48198d30be7cab8
hschoonjans <hschoonjans@cirb.brussels>
2021-10-20 16:06:17 +0200

    Defines a projection; a grails-view like way of providing entity rendering in json and api definition

--- myWorkspace/ex2/src/main/java/com/acme/ex2/repository/BookRepository.java
+++ myWorkspace/ex2/src/main/java/com/acme/ex2/repository/BookRepository.java
@@ -1,8 +1,23 @@
 package com.acme.ex2.repository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.acme.ex2.model.Book;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(excerptProjection = BookRepository.BookProjection.class)
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    interface BookProjection {
+
+        @Value("#{target.id}")
+        Long getId();
+        @Value("#{target.title}")
+        String getTitle();
+        @Value("#{target.author.firstname} #{target.author.lastname}")
+        String getAuthorName();
+        @Value("#{target.category.name}")
+        String getCategoryName();
+    }
 }
