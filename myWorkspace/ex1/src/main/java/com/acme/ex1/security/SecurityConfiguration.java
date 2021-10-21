efd16c5193428ddc93c816325df968506c4517b3
hschoonjans <hschoonjans@cirb.brussels>
2021-10-21 11:46:05 +0200

    Adding security bean

---
+++ myWorkspace/ex1/src/main/java/com/acme/ex1/security/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.acme.ex1.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // code
+}
