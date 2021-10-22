15cbd73b20eebd390c53dd02db84aff2d96a9bd8
hschoonjans <hschoonjans@cirb.brussels>
2021-10-22 15:31:51 +0200

    Ensures the database is initialised

--- myWorkspace/poc-vault/src/main/java/com/acme/poc/vault/ApplicationConfig.java
+++ myWorkspace/poc-vault/src/main/java/com/acme/poc/vault/ApplicationConfig.java
@@ -2,12 +2,15 @@ package com.acme.poc.vault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootApplication
 public class ApplicationConfig {
 
-	public static void main(String[] args) {
-		var ctx = SpringApplication.run(ApplicationConfig.class, args);	
-		ctx.close();
-	}
+    public static void main(String[] args) {
+        var ctx = SpringApplication.run(ApplicationConfig.class, args);
+		// we now have the database
+        System.out.println(ctx.getBean(HikariDataSource.class));
+        ctx.close();
+    }
 }
