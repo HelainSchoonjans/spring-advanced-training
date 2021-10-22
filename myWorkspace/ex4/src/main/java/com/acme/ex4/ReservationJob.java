754f3f7fb99ddf61f174a08ec1d33352449ac488
hschoonjans <hschoonjans@cirb.brussels>
2021-10-22 10:31:17 +0200

    Enables batch processing on ReservationJob

--- myWorkspace/ex4/src/main/java/com/acme/ex4/ReservationJob.java
+++ myWorkspace/ex4/src/main/java/com/acme/ex4/ReservationJob.java
@@ -1,7 +1,9 @@
 package com.acme.ex4;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Configuration;
 
+@EnableBatchProcessing
 @Configuration
 public class ReservationJob {
 
