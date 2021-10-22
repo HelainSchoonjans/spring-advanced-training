c2c3ec5208bf512e3b5b328f69b05e24f5f289ed
hschoonjans <hschoonjans@cirb.brussels>
2021-10-22 10:40:26 +0200

    Defines a reader for the reservations

--- myWorkspace/ex4/src/main/java/com/acme/ex4/ReservationJob.java
+++ myWorkspace/ex4/src/main/java/com/acme/ex4/ReservationJob.java
@@ -1,10 +1,37 @@
 package com.acme.ex4;
 
+import javax.sql.DataSource;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @EnableBatchProcessing
 @Configuration
 public class ReservationJob {
+
+    public static final String RESERVATION_QUERY = """
+            select b.id, b.title, m.username from Reservation r
+            join Member m on r.member_id = m.id
+            join Book b on r.book_id = b.id""";
+
+    @Bean
+    public DataSource getDataSource() {
+        final String url = "jdbc:postgresql://localhost:5432/formation_spring";
+        return new DriverManagerDataSource(url, "postgres", null);
+    }
+
+    @Bean
+    @StepScope
+    public ItemReader<Reservation> reader() {
+        return new JdbcCursorItemReaderBuilder<Reservation>().name("reservationItemReader")
+                .dataSource(getDataSource())
+                .sql(RESERVATION_QUERY)
+                .beanRowMapper(Reservation.class)
+                .build();
+    }
 
 }
