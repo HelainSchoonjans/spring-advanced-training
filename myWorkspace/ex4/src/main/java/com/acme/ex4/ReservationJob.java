902b5105ecc34912eb471b241667cce88fa3bcbb
hschoonjans <hschoonjans@cirb.brussels>
2021-10-22 11:08:12 +0200

    Adds comments of the use of annotations

--- myWorkspace/ex4/src/main/java/com/acme/ex4/ReservationJob.java
+++ myWorkspace/ex4/src/main/java/com/acme/ex4/ReservationJob.java
@@ -1,18 +1,32 @@
 package com.acme.ex4;
 
 import javax.sql.DataSource;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @EnableBatchProcessing
+// @Configuration will make the bean be surrounded by spring proxy
 @Configuration
 public class ReservationJob {
 
@@ -21,6 +35,15 @@ public class ReservationJob {
             join Member m on r.member_id = m.id
             join Book b on r.book_id = b.id""";
 
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public ReservationJob(StepBuilderFactory stepBuilderFactory) {
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    // methods annotated by @Bean will create a bean
+    // calling them is equivalent to ctx.getBean; we don't risk executing them twice
+    // there are executed only once to create the bean
     @Bean
     public DataSource getDataSource() {
         final String url = "jdbc:postgresql://localhost:5432/formation_spring";
